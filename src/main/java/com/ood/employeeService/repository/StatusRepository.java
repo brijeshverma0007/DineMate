574bcdad1e85902c79a20daed61c2d6dc310e805
Brijesh Verma <brijeshverma0007@gmail.com>
2024-05-18 13:36:47 -0400

    Added LoginService

--- src/main/java/com/ood/employeeService/repository/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ood.employeeService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ood.employeeService.model.Status;
-
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-
-}
